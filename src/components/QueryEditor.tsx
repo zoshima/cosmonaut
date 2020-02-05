478b2f4fccbb18b9aba804c2c3d805d5f889117a
zoshima <dmykras@gmail.com>
2020-02-05 16:14:32 +0100

    added groovy syntax highlightling

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import MonacoEditor from "react-monaco-editor";
 import * as monacoEditor from "monaco-editor/esm/vs/editor/editor.api";
+import { registerRulesForLanguage } from "monaco-ace-tokenizer";
+import GremlinHightlightRules from "monaco-ace-tokenizer/lib/ace/definitions/groovy";
 
 const QueryEditor: React.FC<{
   options: any;
   defaultValue: string;
   onChange: any;
 }> = ({ options, onChange, defaultValue }) => {
+  const editorWillMount = (monaco: typeof monacoEditor) => {
+    monaco.languages.register({
+      id: "groovy"
+    });
+
+    registerRulesForLanguage("groovy", new GremlinHightlightRules());
+  };
+
   const editorDidMount = (
     editor: monacoEditor.editor.IStandaloneCodeEditor
   ) => {
@@ -21,6 +31,7 @@ const QueryEditor: React.FC<{
       options={{ ...options, lineNumbers: false }}
       onChange={onChange}
       editorDidMount={editorDidMount}
+      editorWillMount={editorWillMount}
     />
   );
 };
